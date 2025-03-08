3236b151669992e131fbd5358040b1e12c077b31
Salvador Chavez <dchavezvillanueva@gmail.com>
2025-03-08 11:24:43 -0600

    Style: Added colors and changed images to png

---
+++ PokeDex/Colors.swift
@@ -0,0 +1,17 @@
+//
+//  Color.swift
+//  PokeDex
+//
+//  Created by Salvador Chavez on 3/7/25.
+//
+
+import Foundation
+import UIKit
+
+struct Colors {
+    static let pokeBackgroud: Colors = Colors("PokeBackground")
+    static let grass: Colors = Colors("Grass")
+    static let fire: Colors = Colors("Fire")
+    static let water: Colors = Colors("Water")
+    static let darkGray : Colors = Colors("DarkGray")
+}
